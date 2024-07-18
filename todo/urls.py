28c582c599a5664ed90755703cb339e28d6bac50
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-18 14:15:43 +0300

    add urls for all pages

---
+++ todo/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from todo.views import (
+    TaskListView,
+    TagListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskToggleView,
+)
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("task/new/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/edit/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/toggle/", TaskToggleView.as_view(), name="task-toggle"),
+]
+
+app_name = "todo"
