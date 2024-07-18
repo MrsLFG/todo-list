ef3bebc1caf212dae2f27926bf0877385f750e34
Jenya Bondarenko <oleynikjeka@gmail.com>
2024-07-18 13:44:12 +0300

    create admin panel and register models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from todo.models import Task, Tag
+
+
+admin.site.unregister(Group)
+admin.site.register(Tag)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "done", "deadline", ]
+    list_filter = ["deadline", "done", ]
+    search_fields = ["content", ]
